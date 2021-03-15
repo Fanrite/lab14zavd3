22b4b41885fd98775dfea78bd255e968d15c92c4
Fanrite <76695059+Fanrite@users.noreply.github.com>
2021-03-16 01:08:10 +0200

    Добавьте файлы проекта.

---
+++ lab14zavd3/Input.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+
+namespace Inputdata
+{
+    
+    class Input
+    {
+        public static bool Indouble(ref double p, string buk)
+        {
+            string inp;
+
+        vvod:
+            inp = Interaction.InputBox(buk, "vvedenia", $"{p}");
+            try
+            {
+                p = Convert.ToDouble(inp);
+            }
+            catch (System.FormatException)
+            {
+                if (MessageBox.Show("vi vveli ne 4islo" + "\npovtorit?", "uvaga", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                    goto vvod;
+                else
+                    return false;
+            }
+            return true;
+        }
+        public static bool Inint(ref int p, string buk)
+        {
+            string inp;
+
+        vvod:
+            inp = Interaction.InputBox(buk, "vvedenia", $"{p}");
+            try
+            {
+                p = Convert.ToInt32(inp);
+            }
+            catch (System.FormatException)
+            {
+                if (MessageBox.Show("vi vveli ne 4islo" + "\npovtorit?", "uvaga", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                    goto vvod;
+                else
+                    return false;
+            }
+            return true;
+        }
+
+        public static void Printer(int[,] arr, int n, int m, ref string output)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                output += "|";
+                for (int j = 0; j < m; j++)
+                    output += $"{arr[i, j]}|";
+                output += "\n";
+            }
+        }
+
+        public static void Gen(ref int[] arr, int n)
+        {
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+                arr[i] = r.Next(1,9);
+        }
+
+        public static void Printeri(int[] arr, int n, ref string output)
+        {
+            output += "|";
+            for (int i = 0; i < n; i++)
+                output += ($"{arr[i]}|");
+        }
+    }
+}
---
+++ lab14zavd3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+using Inputdata;
+
+namespace lab14zavd3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5, l = 3;
+            bool flag = true;
+            while (flag)
+            {
+                string output = "";
+                
+                Input.Inint(ref n,"Ведіть розмір массиву Х:");
+
+                int[] x = new int[n];
+                Input.Gen(ref x, n);
+                output += "Вхідний массив Х:\n";
+                Input.Printeri(x, n, ref output);
+
+
+                Input.Inint(ref l, "Ведіть ширину рядка:");
+
+                int[,] arr = new int[n, l];
+                int y = 0;
+
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < l; j++)
+                    {
+                        if (y < n) arr[i, j] = x[y];
+                        else arr[i, j] = 0;
+                        y++;
+                    }
+
+
+                output += "\n\nВихідний массив А:\n";
+                Input.Printer(arr, n, l, ref output);
+
+                        //exit or begin 
+                        if (MessageBox.Show(output + "\n\nПовторити?", "Результат", MessageBoxButtons.YesNo) == DialogResult.No)
+                    flag = false;
+            }
+
+        }
+    }
+}
